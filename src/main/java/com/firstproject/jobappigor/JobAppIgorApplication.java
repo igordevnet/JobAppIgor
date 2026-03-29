6df6e425b0d12f9b966accdb4837ad03457a7e8c
IGOR ALMEIDA <igor.almeida55@etec.sp.gov.br>
2026-03-28 22:22:27 -0300

    Understanding Lombok, Servlet and TomCat

---
+++ src/main/java/com/firstproject/jobappigor/JobAppIgorApplication.java
@@ -0,0 +1,13 @@
+package com.firstproject.jobappigor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppIgorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppIgorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/firstproject/jobappigor/model/JobPost.java
@@ -0,0 +1,4 @@
+package com.firstproject.jobappigor;
+
+public class JobPost {
+}
